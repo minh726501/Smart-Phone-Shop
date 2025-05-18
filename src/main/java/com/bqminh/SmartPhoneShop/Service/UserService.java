1986c1bdefee64ffe6a52dcd27c0e6059f074f19
minh726501 <buiminh272002@gmail.com>
2025-05-19 00:54:40 +0900

    update

--- src/main/java/com/bqminh/SmartPhoneShop/Service/UserService.java
+++ src/main/java/com/bqminh/SmartPhoneShop/Service/UserService.java
@@ -3,6 +3,8 @@ package com.bqminh.SmartPhoneShop.Service;
 import com.bqminh.SmartPhoneShop.dto.RegisterDTO;
 import com.bqminh.SmartPhoneShop.enity.Role;
 import com.bqminh.SmartPhoneShop.enity.User;
+import com.bqminh.SmartPhoneShop.repository.OrderRepository;
+import com.bqminh.SmartPhoneShop.repository.ProductRepository;
 import com.bqminh.SmartPhoneShop.repository.RoleRepository;
 import com.bqminh.SmartPhoneShop.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -13,11 +15,16 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
 
-    public UserService(UserRepository userRepository,RoleRepository roleRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, ProductRepository productRepository, OrderRepository orderRepository) {
         this.userRepository = userRepository;
-        this.roleRepository=roleRepository;
+        this.roleRepository = roleRepository;
+        this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
     }
+
     public User saveUser(User user){
         return userRepository.save(user);
     }
@@ -45,6 +52,15 @@ public class UserService {
     }
     public User getUserByEmail(String email){
         return userRepository.findByEmail(email);
+    }
+    public long countUser(){
+        return userRepository.count();
+    }
+    public long countProduct(){
+        return productRepository.count();
+    }
+    public long countOrder(){
+        return orderRepository.count();
     }
 
 }
--- src/main/java/com/bqminh/SmartPhoneShop/controller/admin/DashboardController.java
+++ src/main/java/com/bqminh/SmartPhoneShop/controller/admin/DashboardController.java
@@ -1,13 +1,24 @@
 package com.bqminh.SmartPhoneShop.controller.admin;
 
+import com.bqminh.SmartPhoneShop.Service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DashboardController {
+    private final UserService userService;
+
+    public DashboardController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/admin")
-    public String getDashboard(){
+    public String getDashboard(Model model){
+        model.addAttribute("countUsers",userService.countUser());
+        model.addAttribute("countProducts",userService.countProduct());
+        model.addAttribute("countOrders",userService.countOrder());
         return "admin/dashboard/show";
     }
 
